fcc17210badc2c23de25909f826f0ce2aaa01f95
KubasuIvanSakwa <kubasucuba2020@gmail.com>
2023-07-24 21:24:25 +0300

    Added essentials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import Hero from '../public/Components/Hero'
+import Nav from '../public/Components/Nav'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path="/hero" element={<Hero />} />
+      </Routes> 
+    </BrowserRouter>
   )
 }
 
