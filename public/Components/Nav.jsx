e6a89ed0ae915784128128b4126a5bcfe83bb594
Kubasu Ivan Sakwa <72573043+KubasuIvanSakwa@users.noreply.github.com>
2023-07-26 23:15:17 +0300

    Update Nav.jsx

--- public/Components/Nav.jsx
+++ public/Components/Nav.jsx
@@ -2,11 +2,11 @@ import { NavHashLink as Link } from "react-router-hash-link"
 
 const Nav = () => {
     const links = [
-        {id: 1, name: "HOME", path: ""},
-        {id: 2, name: "ABOUT", path: ""},
-        {id: 3, name: "SERVICES", path: ""},
-        {id: 4, name: "NEWS", path: ""},
-        {id: 5, name: "CONTACT", path: ""}
+        {id: 1, name: "HOME", path: "#"},
+        {id: 2, name: "ABOUT", path: "#"},
+        {id: 3, name: "SERVICES", path: "#"},
+        {id: 4, name: "NEWS", path: "#"},
+        {id: 5, name: "CONTACT", path: "#"}
     ]
     return (
         <nav id="nav">
